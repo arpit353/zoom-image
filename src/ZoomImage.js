92cbdea6c513ae768e4298703654fc421bce8e34
Arpit Khurana <arpit.khurana2015@gmail.com>
2025-01-24 18:29:54 +0530

    changes

--- src/ZoomImage.js
+++ src/ZoomImage.js
@@ -24,16 +24,25 @@ const ZoomImage = () => {
         onMouseLeave={handleMouseLeave}
       >
         <img
-          src="https://fastly.picsum.photos/id/935/200/300.jpg?hmac=XPPjHEBtYb6Y3-p1EjAP0RRB0bNlvqCs52VIysO7iH0"
+          src="https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o"
           alt="Original"
           className="image"
         />
+        {zoomPosition.visible && (
+          <div
+            className="highlight"
+            style={{
+              top: `calc(${zoomPosition.y}% - 50px)`,
+              left: `calc(${zoomPosition.x}% - 50px)`,
+            }}
+          />
+        )}
       </div>
       {zoomPosition.visible && (
         <div
           className="zoomPanel"
           style={{
-            backgroundImage: "url(https://fastly.picsum.photos/id/935/200/300.jpg?hmac=XPPjHEBtYb6Y3-p1EjAP0RRB0bNlvqCs52VIysO7iH0)",
+            backgroundImage: "url(https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o)",
             backgroundPosition: `${zoomPosition.x}% ${zoomPosition.y}%`,
           }}
         />
